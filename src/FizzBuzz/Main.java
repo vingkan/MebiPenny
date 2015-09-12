937546c7e6a87f897552cf907ab5070454283cf7
Vinesh Kannan <vingkan@gmail.com>
2015-09-12 12:59:41 -0500

    Clutched FizzBuzz with 30 seconds left

--- src/FizzBuzz/Main.java
+++ src/FizzBuzz/Main.java
@@ -10,7 +10,6 @@ public class Main {
 	public static void main(String[] args) {
 	
 		
-		
 		BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
 		String line;
 		ArrayList<String> lines = new ArrayList<String>();
@@ -21,8 +20,28 @@ public class Main {
 			}
 		} catch(IOException e) { }
 
-		for (String l : lines) {
+		/*for (String l : lines) {
 			System.out.println(processLine(l));
+		}*/
+		
+		int x = Integer.parseInt(lines.get(0));
+		int y = Integer.parseInt(lines.get(1));
+		int z = Integer.parseInt(lines.get(2));
+		
+		for(int i = 1; i <= x; i++){
+			if(i%y == 0 && i%z == 0){
+				System.out.println("FizzBuzz");
+			}
+			else if(i%y == 0){
+				System.out.println("Fizz");
+			}
+			else if(i%z == 0){
+				System.out.println("Buzz");
+			}
+			else{
+				System.out.println(i);
+			}
+			
 		}
 		
 	}
