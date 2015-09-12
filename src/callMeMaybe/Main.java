39567e48c031d2310cd3fa2d613c5dfad9ac5e7e
Vinesh Kannan <vingkan@gmail.com>
2015-09-11 23:17:30 -0500

    Added code for Call Me Maybe Challenge

---
+++ src/callMeMaybe/Main.java
@@ -0,0 +1,31 @@
+package callMeMaybe;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		ArrayList<String> lines = new ArrayList<String>();
+
+		try {
+			while ((line = bi.readLine()) != null) {
+				lines.add(line);
+			}
+		} catch(IOException e) { }
+
+		for (String l : lines) {
+			System.out.println(processLine(l));
+		}
+	}
+
+	static String processLine(String line) {
+		//Process each line of input here
+		return line;
+	}
+
+}
